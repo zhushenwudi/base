7770aa401658082b3aaed85688e998167a10b6f6
gr.zhu <55681140@163.com>
2021-09-06 12:04:10 +0800

    v1.1.4 add 顶部提示 view

---
+++ base/src/main/java/com/zhushenwudi/base/view/TopNiftyView.java
@@ -0,0 +1,380 @@
+package com.zhushenwudi.base.view;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.ViewPropertyAnimatorListenerAdapter;
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
+
+import com.zhushenwudi.base.R;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+public class TopNiftyView {
+    public static final int LENGTH_SHORT = -1;
+    private final ViewGroup mParent;
+    private final Context mContext;
+    private final TopNiftyView.SnackbarLayout mView;
+    private final TopNiftyViewManager.Callback mManagerCallback = new TopNiftyViewManager.Callback() {
+        public void show() {
+            TopNiftyView.sHandler.sendMessage(TopNiftyView.sHandler.obtainMessage(0, TopNiftyView.this));
+        }
+
+        public void dismiss() {
+            TopNiftyView.sHandler.sendMessage(TopNiftyView.sHandler.obtainMessage(1, TopNiftyView.this));
+        }
+    };
+    private static final Handler sHandler = new Handler(Looper.getMainLooper(), message -> {
+        switch (message.what) {
+            case 0:
+                ((TopNiftyView) message.obj).showView();
+                return true;
+            case 1:
+                ((TopNiftyView) message.obj).hideView();
+                return true;
+            default:
+                return false;
+        }
+    });
+    private int mDuration;
+
+    TopNiftyView(ViewGroup parent) {
+        this.mParent = parent;
+        this.mContext = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(this.mContext);
+        this.mView = (TopNiftyView.SnackbarLayout) inflater.inflate(R.layout.layout_snackbar, this.mParent, false);
+    }
+
+    /**
+     * @param activity  The Activity
+     * @param text      The msg to show
+     * @param duration  notify time
+     * @param marginTop margin to the top of main content
+     * @return TopNiftyView
+     */
+    public static TopNiftyView make(Activity activity, CharSequence text, int duration, int marginTop) {
+        TopNiftyView snackbar;
+        final RelativeLayout layout = new RelativeLayout(activity);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, dip2px(activity, marginTop + 80));
+        layout.setLayoutParams(params);
+
+        if (marginTop != 0) {
+            final RelativeLayout layout1 = new RelativeLayout(activity);
+            RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, dip2px(activity, 80));
+            params1.setMargins(0, dip2px(activity, marginTop), 0, 0); //距离上边actionBarHeight dp
+            layout1.setLayoutParams(params1);
+            layout1.setGravity(Gravity.CENTER_HORIZONTAL);
+            layout.addView(layout1);
+            activity.addContentView(layout, params);
+            snackbar = new TopNiftyView(findSuitableParent(layout1));
+        } else {
+            activity.addContentView(layout, params);
+            snackbar = new TopNiftyView(findSuitableParent(layout));
+        }
+
+        snackbar.setText(text);
+        snackbar.setDuration(duration);
+        return snackbar;
+    }
+
+    public static TopNiftyView make(Activity activity, int resId, int duration, int actionBarHeight) {
+        return make(activity, activity.getResources().getText(resId), duration, actionBarHeight);
+    }
+
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    @Nullable
+    private static ViewGroup findSuitableParent(View view) {
+        return (ViewGroup) view;
+    }
+
+    public TopNiftyView setAction(@StringRes int resId, OnClickListener listener) {
+        return this.setAction(this.mContext.getText(resId), listener);
+    }
+
+    public TopNiftyView setAction(CharSequence text, final OnClickListener listener) {
+        TextView tv = this.mView.getActionView();
+        if (!TextUtils.isEmpty(text) && listener != null) {
+            tv.setVisibility(View.VISIBLE);
+            tv.setText(text);
+            tv.setOnClickListener(view -> {
+                listener.onClick(view);
+                TopNiftyView.this.dismiss();
+            });
+        } else {
+            tv.setVisibility(View.GONE);
+            tv.setOnClickListener(null);
+        }
+        return this;
+    }
+
+    public TopNiftyView setActionTextColor(ColorStateList colors) {
+        TextView tv = this.mView.getActionView();
+        tv.setTextColor(colors);
+        return this;
+    }
+
+    public TopNiftyView setActionTextColor(int color) {
+        TextView tv = this.mView.getActionView();
+        tv.setTextColor(color);
+        return this;
+    }
+
+    public TopNiftyView setTextColor(ColorStateList colors) {
+        TextView tv = this.mView.getMessageView();
+        tv.setTextColor(colors);
+        return this;
+    }
+
+    public TopNiftyView setTextColor(int color) {
+        TextView tv = this.mView.getMessageView();
+        tv.setTextColor(color);
+        return this;
+    }
+
+    public TopNiftyView setText(CharSequence message) {
+        TextView tv = this.mView.getMessageView();
+        tv.setText(message);
+        return this;
+    }
+
+    public TopNiftyView setBackground(int color) {
+        this.mView.setBackgroundColor(color);
+        return this;
+    }
+
+    public TopNiftyView setBackground2(int res) {
+        this.mView.setBackgroundResource(res);
+        return this;
+    }
+
+    public TopNiftyView setText(@StringRes int resId) {
+        return this.setText(this.mContext.getText(resId));
+    }
+
+    public int getDuration() {
+        return this.mDuration;
+    }
+
+    public TopNiftyView setDuration(int duration) {
+        this.mDuration = duration;
+        return this;
+    }
+
+    public View getView() {
+        return this.mView;
+    }
+
+    public void show() {
+        TopNiftyViewManager.getInstance().show(this.mDuration, this.mManagerCallback);
+    }
+
+    public void dismiss() {
+        TopNiftyViewManager.getInstance().dismiss(this.mManagerCallback);
+    }
+
+    final void showView() {
+        if (this.mView.getParent() == null) {
+            this.mParent.addView(this.mView);
+        }
+        if (ViewCompat.isLaidOut(this.mView)) {
+            this.animateViewIn();
+        } else {
+            this.mView.setOnLayoutChangeListener((view, left, top, right, bottom) -> {
+                TopNiftyView.this.animateViewIn();
+                TopNiftyView.this.mView.setOnLayoutChangeListener(null);
+            });
+        }
+    }
+
+    private void animateViewIn() {
+        ViewCompat.setTranslationY(this.mView, -(float) this.mView.getHeight());
+        ViewCompat.animate(this.mView).translationY(0.0F).setInterpolator(new FastOutSlowInInterpolator()).setDuration(500L).setListener(new ViewPropertyAnimatorListenerAdapter() {
+            public void onAnimationStart(View view) {
+                TopNiftyView.this.mView.animateChildrenIn(70, 180);
+            }
+
+            public void onAnimationEnd(View view) {
+                TopNiftyViewManager.getInstance().onShown(TopNiftyView.this.mManagerCallback);
+            }
+        }).start();
+    }
+
+    private void animateViewOut() {
+        ViewCompat.animate(this.mView).translationY(-(float) this.mView.getHeight()).setInterpolator(new FastOutSlowInInterpolator()).setDuration(500L).setListener(new ViewPropertyAnimatorListenerAdapter() {
+            public void onAnimationStart(View view) {
+                TopNiftyView.this.mView.animateChildrenOut(0, 180);
+            }
+
+            public void onAnimationEnd(View view) {
+                TopNiftyView.this.onViewHidden();
+            }
+        }).start();
+
+    }
+
+    final void hideView() {
+        if (this.mView.getVisibility() == View.VISIBLE) {
+            this.animateViewOut();
+        } else {
+            this.onViewHidden();
+        }
+    }
+
+    private void onViewHidden() {
+        this.mParent.removeView(this.mView);
+        TopNiftyViewManager.getInstance().onDismissed(this.mManagerCallback);
+    }
+
+
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Duration {
+    }
+
+    public static class SnackbarLayout extends LinearLayout {
+        private TextView mMessageView;
+        private TextView mActionView;
+        private int mMaxWidth;
+        private int mMaxInlineActionWidth;
+        private TopNiftyView.SnackbarLayout.OnLayoutChangeListener mOnLayoutChangeListener;
+
+        public SnackbarLayout(Context context) {
+            this(context, null);
+        }
+
+        @SuppressLint("PrivateResource")
+        public SnackbarLayout(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SnackbarLayout);
+            this.mMaxWidth = a.getDimensionPixelSize(R.styleable.SnackbarLayout_android_maxWidth, -1);
+            this.mMaxInlineActionWidth = a.getDimensionPixelSize(R.styleable.SnackbarLayout_maxActionInlineWidth, -1);
+            if (a.hasValue(R.styleable.SnackbarLayout_elevation)) {
+                ViewCompat.setElevation(this, (float) a.getDimensionPixelSize(R.styleable.SnackbarLayout_elevation, 0));
+            }
+
+            a.recycle();
+            this.setClickable(true);
+            LayoutInflater.from(context).inflate(R.layout.layout_snackbar_include, this);
+        }
+
+        private static void updateTopBottomPadding(View view, int topPadding, int bottomPadding) {
+            if (ViewCompat.isPaddingRelative(view)) {
+                ViewCompat.setPaddingRelative(view, ViewCompat.getPaddingStart(view), topPadding, ViewCompat.getPaddingEnd(view), bottomPadding);
+            } else {
+                view.setPadding(view.getPaddingLeft(), topPadding, view.getPaddingRight(), bottomPadding);
+            }
+        }
+
+        protected void onFinishInflate() {
+            super.onFinishInflate();
+            this.mMessageView = this.findViewById(R.id.snackbar_text);
+            this.mActionView = this.findViewById(R.id.snackbar_action);
+        }
+
+        TextView getMessageView() {
+            return this.mMessageView;
+        }
+
+        TextView getActionView() {
+            return this.mActionView;
+        }
+
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            if (this.mMaxWidth > 0 && this.getMeasuredWidth() > this.mMaxWidth) {
+                widthMeasureSpec = MeasureSpec.makeMeasureSpec(this.mMaxWidth, MeasureSpec.EXACTLY);
+                super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            }
+
+            int multiLineVPadding = this.getResources().getDimensionPixelSize(R.dimen.snackbar_padding_vertical_2lines);
+            int singleLineVPadding = this.getResources().getDimensionPixelSize(R.dimen.snackbar_padding_vertical);
+            boolean isMultiLine = this.mMessageView.getLayout().getLineCount() > 1;
+            boolean remeasure = false;
+            if (isMultiLine && this.mMaxInlineActionWidth > 0 && this.mActionView.getMeasuredWidth() > this.mMaxInlineActionWidth) {
+                if (this.updateViewsWithinLayout(1, multiLineVPadding, multiLineVPadding - singleLineVPadding)) {
+                    remeasure = true;
+                }
+            } else {
+                int messagePadding = isMultiLine ? multiLineVPadding : singleLineVPadding;
+                if (this.updateViewsWithinLayout(0, messagePadding, messagePadding)) {
+                    remeasure = true;
+                }
+            }
+
+            if (remeasure) {
+                super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            }
+        }
+
+        void animateChildrenIn(int delay, int duration) {
+            ViewCompat.setAlpha(this.mMessageView, 0.0F);
+            ViewCompat.animate(this.mMessageView).alpha(1.0F).setDuration(duration).setStartDelay(delay).start();
+            if (this.mActionView.getVisibility() == VISIBLE) {
+                ViewCompat.setAlpha(this.mActionView, 0.0F);
+                ViewCompat.animate(this.mActionView).alpha(1.0F).setDuration(duration).setStartDelay(delay).start();
+            }
+
+        }
+
+        void animateChildrenOut(int delay, int duration) {
+            ViewCompat.setAlpha(this.mMessageView, 1.0F);
+            ViewCompat.animate(this.mMessageView).alpha(0.0F).setDuration(duration).setStartDelay(delay).start();
+            if (this.mActionView.getVisibility() == VISIBLE) {
+                ViewCompat.setAlpha(this.mActionView, 1.0F);
+                ViewCompat.animate(this.mActionView).alpha(0.0F).setDuration(duration).setStartDelay(delay).start();
+            }
+        }
+
+        protected void onLayout(boolean changed, int l, int t, int r, int b) {
+            super.onLayout(changed, l, t, r, b);
+            if (changed && this.mOnLayoutChangeListener != null) {
+                this.mOnLayoutChangeListener.onLayoutChange(this, l, t, r, b);
+            }
+        }
+
+        void setOnLayoutChangeListener(TopNiftyView.SnackbarLayout.OnLayoutChangeListener onLayoutChangeListener) {
+            this.mOnLayoutChangeListener = onLayoutChangeListener;
+        }
+
+        private boolean updateViewsWithinLayout(int orientation, int messagePadTop, int messagePadBottom) {
+            boolean changed = false;
+            if (orientation != this.getOrientation()) {
+                this.setOrientation(orientation);
+                changed = true;
+            }
+
+            if (this.mMessageView.getPaddingTop() != messagePadTop || this.mMessageView.getPaddingBottom() != messagePadBottom) {
+                updateTopBottomPadding(this.mMessageView, messagePadTop, messagePadBottom);
+                changed = true;
+            }
+
+            return changed;
+        }
+
+        interface OnLayoutChangeListener {
+            void onLayoutChange(View var1, int var2, int var3, int var4, int var5);
+        }
+    }
+}
---
+++ base/src/main/java/com/zhushenwudi/base/view/TopNiftyViewManager.java
@@ -0,0 +1,176 @@
+package com.zhushenwudi.base.view;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import java.lang.ref.WeakReference;
+
+class TopNiftyViewManager {
+    private static final int MSG_TIMEOUT = 0;
+    private static final int SHORT_DURATION_MS = 1500;
+    private static final int LONG_DURATION_MS = 2750;
+    private static TopNiftyViewManager sSnackbarManager;
+    private final Object mLock = new Object();
+    private SnackbarRecord mCurrentSnackbar;
+    private SnackbarRecord mNextSnackbar;
+    private final Handler mHandler = new Handler(Looper.getMainLooper(), new Handler.Callback() {
+        public boolean handleMessage(Message message) {
+            if (message.what == 0) {
+                TopNiftyViewManager.this.handleTimeout((SnackbarRecord) message.obj);
+                return true;
+            }
+            return false;
+        }
+    });
+
+    private TopNiftyViewManager() {
+    }
+
+    static TopNiftyViewManager getInstance() {
+        if (sSnackbarManager == null) {
+            sSnackbarManager = new TopNiftyViewManager();
+        }
+
+        return sSnackbarManager;
+    }
+
+    public void show(int duration, Callback callback) {
+        synchronized (this.mLock) {
+            if (this.isCurrentSnackbar(callback)) {
+                this.mCurrentSnackbar.duration = duration;
+                this.mHandler.removeCallbacksAndMessages(this.mCurrentSnackbar);
+                this.scheduleTimeoutLocked(this.mCurrentSnackbar);
+            } else {
+                if (this.isNextSnackbar(callback)) {
+                    this.mNextSnackbar.duration = duration;
+                } else {
+                    this.mNextSnackbar = new SnackbarRecord(duration, callback);
+                }
+
+                if (this.mCurrentSnackbar == null || !this.cancelSnackbarLocked(this.mCurrentSnackbar)) {
+                    this.mCurrentSnackbar = null;
+                    this.showNextSnackbarLocked();
+                }
+            }
+        }
+    }
+
+    public void dismiss(Callback callback) {
+        synchronized (this.mLock) {
+            if (this.isCurrentSnackbar(callback)) {
+                this.cancelSnackbarLocked(this.mCurrentSnackbar);
+            }
+
+            if (this.isNextSnackbar(callback)) {
+                this.cancelSnackbarLocked(this.mNextSnackbar);
+            }
+
+        }
+    }
+
+    public void onDismissed(Callback callback) {
+        synchronized (this.mLock) {
+            if (this.isCurrentSnackbar(callback)) {
+                this.mCurrentSnackbar = null;
+                if (this.mNextSnackbar != null) {
+                    this.showNextSnackbarLocked();
+                }
+            }
+
+        }
+    }
+
+    public void onShown(Callback callback) {
+        synchronized (this.mLock) {
+            if (this.isCurrentSnackbar(callback)) {
+                this.scheduleTimeoutLocked(this.mCurrentSnackbar);
+            }
+
+        }
+    }
+
+    public void cancelTimeout(Callback callback) {
+        synchronized (this.mLock) {
+            if (this.isCurrentSnackbar(callback)) {
+                this.mHandler.removeCallbacksAndMessages(this.mCurrentSnackbar);
+            }
+
+        }
+    }
+
+    public void restoreTimeout(Callback callback) {
+        synchronized (this.mLock) {
+            if (this.isCurrentSnackbar(callback)) {
+                this.scheduleTimeoutLocked(this.mCurrentSnackbar);
+            }
+
+        }
+    }
+
+    private void showNextSnackbarLocked() {
+        if (this.mNextSnackbar != null) {
+            this.mCurrentSnackbar = this.mNextSnackbar;
+            this.mNextSnackbar = null;
+            Callback callback = this.mCurrentSnackbar.callback.get();
+            if (callback != null) {
+                callback.show();
+            } else {
+                this.mCurrentSnackbar = null;
+            }
+        }
+
+    }
+
+    private boolean cancelSnackbarLocked(SnackbarRecord record) {
+        Callback callback = record.callback.get();
+        if (callback != null) {
+            callback.dismiss();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isCurrentSnackbar(Callback callback) {
+        return this.mCurrentSnackbar != null && this.mCurrentSnackbar.isSnackbar(callback);
+    }
+
+    private boolean isNextSnackbar(Callback callback) {
+        return this.mNextSnackbar != null && this.mNextSnackbar.isSnackbar(callback);
+    }
+
+    private void scheduleTimeoutLocked(SnackbarRecord r) {
+        this.mHandler.removeCallbacksAndMessages(r);
+        this.mHandler.sendMessageDelayed(Message.obtain(this.mHandler, 0, r), r.duration == 0 ? 2750L : 1500L);
+    }
+
+    private void handleTimeout(SnackbarRecord record) {
+        synchronized (this.mLock) {
+            if (this.mCurrentSnackbar == record || this.mNextSnackbar == record) {
+                this.cancelSnackbarLocked(record);
+            }
+
+        }
+    }
+
+    interface Callback {
+        void show();
+
+        void dismiss();
+    }
+
+    private static class SnackbarRecord {
+        private final WeakReference<Callback> callback;
+        private int duration;
+
+        SnackbarRecord(int duration, Callback callback) {
+            this.callback = new WeakReference(callback);
+            this.duration = duration;
+        }
+
+        boolean isSnackbar(Callback callback) {
+            return callback != null && this.callback.get() == callback;
+        }
+    }
+}
